11b8e8c65b81011e374955ea5ec039576781b755
kossaychargui <kossay134@gmail.com>
2023-03-16 16:29:38 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
  * main - the entry point in the program
  * void : no arguments
  *
- * return 0: the main fuction returns 0
+ * Return (0): seccess
  */
 int main(void)
 {
 	puts("\"Programming is like building a multiligual puzzle\n");
-	return 0;
+	return (0);
 }
