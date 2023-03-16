164655abb1c157970a5af7f6cd81a47d19c39545
kossaychargui <kossay134@gmail.com>
2023-03-16 16:09:01 +0000

    printing in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
+/**
+ * main - the entry point in the program
+ * void : no arguments
+ *
+ * return 0: the main fuction returns 0
+ */
 int main(void)
 {
-	puts("Programming is like building a multiligual puzzle\n");
+	puts("Programming is like building a multiligual puzzle \n");
 	return 0;
 }
