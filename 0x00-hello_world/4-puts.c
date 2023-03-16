af15594e59dc111b25726dfc42237f3ac32426d6
kossaychargui <kossay134@gmail.com>
2023-03-16 16:54:10 +0000

    taks 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 /**
- * main - A program that prints a line with puts fuctio
- * n
+ * main - A program that prints a line with puts fuction
  *
  * Return: 0 (seccess)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multiligual puzzle");
-        return (0);
+	return (0);
 }
