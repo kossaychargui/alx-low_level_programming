e7cb4d5608a6fe9fd8a9ed5b68bc764ab951700c
kossaychargui <kossay134@gmail.com>
2023-03-20 11:49:41 +0000

    printing all digits from 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - function that prints all the digits from 0 to 10
+ *
+ *Return: (0) success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
