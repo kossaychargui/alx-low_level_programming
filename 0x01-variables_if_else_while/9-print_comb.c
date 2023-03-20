b9f23dfb69403c1cf3e14a5b13fce49d1bb3ab6a
kossaychargui <kossay134@gmail.com>
2023-03-20 13:28:16 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - function that prints all single digits from 0 to 10 seperated
+ *
+ *Return: (0)  success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58 ; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
