df32e110bdbc50a457b0bafde600bb5716db053f
kossaychargui <kossay134@gmail.com>
2023-03-21 23:44:52 +0000

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - function that comutes the absolute value of an integer
+ *
+ * Return: returns the absolue of the integer
+ */
+int _abs(int x)
+{
+	return (abs(x));
+}
+	
+	
