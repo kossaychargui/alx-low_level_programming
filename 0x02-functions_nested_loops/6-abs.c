cffea739074a18ce35574c9e5a1b5bb6e1c2c4c1
kossaychargui <kossay134@gmail.com>
2023-03-21 23:47:50 +0000

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,11 +4,11 @@
 /**
  * _abs - function that comutes the absolute value of an integer
  *
+ * @x: integer
+ *
  * Return: returns the absolue of the integer
  */
 int _abs(int x)
 {
 	return (abs(x));
 }
-	
-	
