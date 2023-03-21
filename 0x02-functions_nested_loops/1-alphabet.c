657f01d35c15f2713c8b331733ff77c3b22fe7db
kossaychargui <kossay134@gmail.com>
2023-03-21 21:43:23 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *main - fucntion that prints alphabet
+ *
+ * Return: (0) success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
