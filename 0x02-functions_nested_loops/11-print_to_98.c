322446398621a6917494dd6a45a5cdf20c190de6
kossaychargui <kossay134@gmail.com>
2023-03-22 02:14:18 +0000

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(i + '0');
+		if (i == (n - 1))
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
