d422febf190bbde486324d8f6a5a224b844afcf3
kossaychargui <kossay134@gmail.com>
2023-03-22 00:56:36 +0000

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,9 @@
 #include "main.h"
+
+/**
+ * jack_bauer - function that prints every minute of the day
+ *
+ */
 void jack_bauer(void)
 {
 	int i, j;
@@ -7,9 +12,11 @@ void jack_bauer(void)
 	{
 		for (j = 0; j < 60; j++)
 		{
-			_putchar(i + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar(j + '0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
 		}
 	}
