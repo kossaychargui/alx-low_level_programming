b027c5f07da1642759ca9ca20f05c21c9813fa67
kossaychargui <kossay134@gmail.com>
2023-03-22 23:24:25 +0000

    task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,4 +7,8 @@ void positve_or_negarive(int i);
 
 int largest_number(int a, int b, int c);
 
+void print_remaining_days(int month, int day, int year);
+
+int convert_day(int month, int day);
+
 #endif /* MAIN_H */
