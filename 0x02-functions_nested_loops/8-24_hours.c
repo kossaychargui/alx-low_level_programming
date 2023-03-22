101179265a9e302846f48f3d031236897e79e9a8
kossaychargui <kossay134@gmail.com>
2023-03-22 00:41:38 +0000

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include "main.h"
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24 ; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i + '0');
+			_putchar(':');
+			_putchar(j + '0');
+			_putchar('\n');
+		}
+	}
+}
