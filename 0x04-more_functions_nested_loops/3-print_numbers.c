49497d8688d95840c999e1e6439133b606534aa3
kossaychargui <kossay134@gmail.com>
2023-03-23 17:23:43 +0000

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints numbers for 0 to 9
+ *
+ * @void: no argument
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
