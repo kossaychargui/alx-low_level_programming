39f7170f88fab7666acd92de06c57a3ade944e65
kossaychargui <kossay134@gmail.com>
2023-03-23 23:38:50 +0000

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints numbers from 0 to 14 x10
+ *
+ * @void: no argument
+ *
+ */
+void more_numbers(void)
+{
+	int j, i;
+
+	j = 0;
+
+	while (j < 10)
+	{
+
+		for (i = 0; i <= 57; i++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
+
