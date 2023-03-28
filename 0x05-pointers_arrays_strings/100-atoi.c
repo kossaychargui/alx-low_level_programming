ebdd6fa2ef5dbe7cc83e68f5c4fa922182c3fb7b
kossaychargui <kossay134@gmail.com>
2023-03-28 20:02:40 +0000

    task 103

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * _atoi - function that convert a string to an integer
+ *
+ * @s: string to convert
+ *
+ * Return: the converted integer
+ *
+ */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
