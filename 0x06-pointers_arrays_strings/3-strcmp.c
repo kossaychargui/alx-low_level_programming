f7ace185da4bea400c93f5acbdf128116153e026
kossaychargui <kossay134@gmail.com>
2023-03-29 16:18:59 +0000

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ * _strcmp - function that compates two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 if s1 and s2 are exactly the same otherwise returns number
+ * diffirent to 0 .
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
