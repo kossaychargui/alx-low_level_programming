00233c83e8d85a5f5d2f949326dd964875eddcaf
kossaychargui <kossay134@gmail.com>
2023-04-03 23:08:51 +0000

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include <string.h>
+
+/**
+ * _strchr - function that locates a character in a string
+ *
+ * @s: string to locate in
+ * @c: charachter to locate
+ *
+ * Return: the first occurance of c in s otherwise NULL .
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	char *s1;
+
+	s1 = strchr(s, c);
+	if (s1 != NULL)
+	{
+		return (s);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
