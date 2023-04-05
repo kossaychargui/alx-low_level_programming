bba2823a1391f085e005e847d8334a92129b1df6
kossaychargui <kossay134@gmail.com>
2023-04-05 22:27:08 +0000

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,3 +1,4 @@
+#include <string.h>
 /**
  * _print_rev_recursion - function that prints a string in reverse
  *
@@ -9,7 +10,7 @@ void _print_rev_recursion(char *s)
 	int len;
 
 	len = strlen(s) - 1;
-	_putchar(*(s + len));
+	putchar(*(s + len));
 	s--;
 	_print_rev_recursion(s);
 }
