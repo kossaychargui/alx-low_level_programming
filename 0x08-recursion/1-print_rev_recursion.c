e0bae0329fc543448d2978fd9acff19d6eb2a854
kossaychargui <kossay134@gmail.com>
2023-04-05 22:04:07 +0000

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ *
+ *@s: string to print
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+	int len;
+
+	len = strlen(s) - 1;
+	_putchar(*(s + len));
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _strlen_recursion - function that returns the length of a string
+ *
+ * @s: string
+ *
+ * Return: the lenght of the string.
+ *
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
