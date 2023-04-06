6be06bab8db6a54353f49018c046c19a8e63fa03
kossaychargui <kossay134@gmail.com>
2023-04-06 00:36:41 +0000

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <math.h>
 
 /**
  * _sqrt_recrusion - function that returns the square root of a number
@@ -10,23 +11,5 @@
  */
 int _sqrt_recrusion(int n)
 {
-	int i, s;
-
-	s = 0;
-
-	for (i = 0; i < n; i++)
-	{
-		if (i * i == n)
-		{
-			s = i;
-		}
-	}
-	if (s != 0)
-	{
-		return (s);
-	}
-	else
-	{
-		return (-1);
-	}
+	return (sqrt(n));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,7 +9,7 @@
 int is_prime_number(int n)
 {
 	int i, prime;
-	
+
 	prime = 1;
 	for (i = 2; i <= n / 2; i++)
 	{
