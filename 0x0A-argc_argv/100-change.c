e9e0447813c5420921b0b4c28485ccb19b593e87
kossaychargui <kossay134@gmail.com>
2023-04-10 22:23:01 +0000

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * give_cents - function that returns the minumum number of coins to make
+ * change for an amount of money.
+ *
+ * @coins: the amount of money
+ *
+ * Return: cents, the amount of cents to give.
+ */
 int give_cents(int coins)
 {
 	int cents, c1, c2, c3, c4, c5;
@@ -22,14 +30,24 @@ int give_cents(int coins)
 		c4 = coins - c3 * 5;
 	}
 	c5 = (coins - c4 * 2) / 1;
-        
 	cents = c1 + c2 + c3 + c4 + c5;
 	return (cents);
 }
+
+/**
+ * main - entry point of the code . a program that prints the minumum number
+ * of coins to make change for and amount of money.
+ *
+ * @argc: number of arguments passed in the command line.
+ * @argv: array of strings holds the arguments passed in the command line.
+ *
+ * Return: 1 if the number of arguments passed is not exactly 1 otherwise 0 .
+ *
+ */
 int main(int argc, char *argv[])
 {
 	int coins, cents;
-	
+
 	if (argc != 2)
 	{
 		printf("Error\n");
