587d27d73169ea0d153b75e3aaf9173e63ddc3ff
kossaychargui <kossay134@gmail.com>
2023-04-10 01:29:53 +0000

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,9 +3,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * check - function that check if the string is a numeric .
+ *
+ * @str: string to check
+ *
+ * Return: 1 if the string is not numeric otherwise 0
+ *
+ */
 int check(char str[])
 {
 	int i, chek, len;
+
 	chek = 0;
 	len = strlen(str);
 	for (i = 0; i < len; i++)
@@ -24,7 +33,14 @@ int check(char str[])
 		return (0);
 	}
 }
-
+/**
+ * main - entry point of the program
+ *
+ * @argc: number of arguments passed in the command line
+ * @argv: array of strings contains the arguments passed in the command line
+ * Return: 1 if one of the numbers contains a non digit symbol otherwise 0 .
+ *
+ */
 int main(int argc, char *argv[])
 {
 	int i, j, sum;
