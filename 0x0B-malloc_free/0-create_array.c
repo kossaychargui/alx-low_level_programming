4d427da85b9cd79c754275d435aa379b5460b1dd
kossaychargui <kossay134@gmail.com>
2023-04-11 22:48:13 +0000

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,6 +14,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str;
+	int i;
 
 	if (size == 0)
 	{
@@ -25,6 +26,9 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	*str = c;
+	for (i = 0; i < size; i++)
+	{
+		*(str + i) = c;
+	}
 	return (str);
 }
