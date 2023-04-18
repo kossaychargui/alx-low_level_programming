7e5aa575dde445bff19beefecdb96e6db0c8f242
kossaychargui <kossay134@gmail.com>
2023-04-18 02:38:36 +0000

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  * @owner: string holds the dog's owner.
  *
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
