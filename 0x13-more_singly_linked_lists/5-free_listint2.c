cfac798ed7e73b6afcf48628af9fdd1e153333ae
kossaychargui <kossay134@gmail.com>
2023-04-30 17:59:42 +0000

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,7 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current, *next, *new_head __attribute__((unused));
+	listint_t *current, *next;
 
 	if (head == NULL)
 	{
@@ -18,14 +18,13 @@ void free_listint2(listint_t **head)
 	else
 	{
 		current = (*head)->next;
-		new_head = *head;
 		while (current != NULL)
 		{
 			next = current->next;
 			free(current);
 			current = next;
 		}
-		free(new_head);
-		new_head = NULL;
+		free(*head);
+		*head = NULL;
 	}
 }
