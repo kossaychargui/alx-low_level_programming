64d1177dd7ac85018ec51a7c6e8aef104043126a
kossaychargui <kossay134@gmail.com>
2023-05-01 16:08:01 +0000

    task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -12,10 +12,18 @@ void free_list(list_t *head)
 	list_t *current, *next;
 
 	current = head;
-	while (current != NULL)
+	if (current == NULL)
 	{
-		next = current->next;
-		free(current);
-		current = next;
+		return;
 	}
-}
+	else
+	{
+		while (current != NULL)
+		{
+			next = current->next;
+			free(current->str);
+			free(current);
+			current = next;
+		}
+	}
+}	
