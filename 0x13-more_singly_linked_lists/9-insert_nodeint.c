f77652927e06d32d905cd6c346a6bcbe8c785209
kossaychargui <kossay134@gmail.com>
2023-05-01 15:04:02 +0000

    task 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		while (current != NULL)
 		{
-			if ((count + 1) == idx)
+			if (count == idx)
 			{
 				new->next = current->next;
 				current->next = new;
