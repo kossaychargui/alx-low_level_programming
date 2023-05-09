4342a6af4d071adbf458efa1efcdbb40e823761b
kossaychargui <kossay134@gmail.com>
2023-05-09 02:28:45 +0000

    task 4

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program. program that copies the contentof a file
+ * to another file.
+ *
+ * @argc: argument counter .
+ * @argv: array of string holds the commands entred in the command line.
+ *
+ * Return: 0 success.
+ */
+int main(int argc, char **argv)
+{
+	FILE *f1, *f2;
+
+	if (argc != 2)
+	{
+		printf("Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	f1 = fopen(argv[1], "w+");
+	if (f1 == NULL)
+	{
+		printf("Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	f2 = fopen(argv[2], "w");
+	if (f2 == NULL)
+	{
+		printf("Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	f2 = f1;
+	resutl1 = fclose(f1);
+	if (result1 == -1)
+	{
+		printf("Error: Can't close fd %d\n", result1);
+	}
+	resutl2 = fclose(f2);
+	if (result2 == -1)
+	{
+		printf("Error: Can't close fd %d\n", restult2);
+	}
+	return (0);
+}
