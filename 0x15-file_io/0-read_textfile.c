631f27804928cc3129de573040f93995bcef3eb3
kossaychargui <kossay134@gmail.com>
2023-05-09 01:01:34 +0000

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * read_textfile - function that reads and prints a file to the POSTIX standard
+ * output.
+ *
+ * @filename: the file name.
+ * @letters: the number of letters it should read and print.
+ *
+ * Return: the actual number of letter it could read and print, 0 if failed.
+ *
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fptr;
+	ssize_t size = letters;
+	char *buffer;
+	ssize_t count;
+	int i;
+
+	buffer = malloc(sizeof(char) * size);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+	count = 0;
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fptr = fopen(filename, "r");
+	if (fptr == NULL)
+	{
+		return (0);
+	}
+	while (fgets(buffer, size, fptr) != NULL && count < size)
+	{
+		i = 0;
+		while (buffer[i] != '\0')
+		{
+			printf("%c", buffer[i]);
+			i++;
+			count++;
+		}
+	}
+	fclose(fptr);
+	return (count);
+}
